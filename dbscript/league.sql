8d67cbb5f845f93fee50b92e2dc2c427433e7210
onnanapiando <onnapiando@gmail.com>
2014-08-11 20:06:13 +0700

    Added league.sql

---
+++ dbscript/league.sql
@@ -0,0 +1,55 @@
+create table league (
+     league_ id int primary key,
+     league_name text,
+     sport_type text,
+     fixture_type text
+);
+
+--create language plpgsql;
+-- controller
+create or replace 
+    function setleague(p_league_id int, p_league_name text,p_sport_type text, p_fixture_type text) 
+    returns text as
+$$
+  declare
+     v_league_id int;
+  begin
+      select into v_league_id league_id from league 
+         where league_id = p_league_id;
+         
+      if v_league_id isnull then
+          insert into league(league_id, league_name, sport_type,fixture_type) values
+             (p_league_id, p_league_name, p_sport_type,p_fixture_type);
+      else
+           update League
+           set league_name = p_league_name
+           where league_id = p_league_id;
+
+           update League
+           set sport_type = p_sport_type
+           where league_id = p_league_id;
+
+           update League
+           set fixture_type = p_fixture_type
+           where league_id = p_league_id;
+
+      end if;   
+         
+      return 'OK';
+  end;
+$$
+  language 'plpgsql'; 
+
+--HOW TO USE:
+-- SELECT setleague(1, 'first time');
+
+--view
+create or replace function 
+    get_league_perid(in int, out text, out text, out text) 
+returns setof record as
+$$ 
+     select  league_name, sport_type,fixture_type from league
+     where league_id = $1;
+     
+$$
+ language 'sql';
