5d1a3bef5aef2db6521b2cbdc062eb57c6fd50dc
Kay Apao <kayapao@yahoo.com>
2014-08-12 21:36:46 +0800

    The addition of variables for start-end time and the scores of the teams, and the changing of data type for time

--- dbscript/event.sql
+++ dbscript/event.sql
@@ -2,20 +2,22 @@ create table events(
     e_id int primary key,
     eDate text,
     eLocation text,
-    eTime text,
-    scores int
+    eTime_s time,
+	eTime_e time,
+    scoret1 int,
+	scoret2 int
 );
 
 --HOW TO USE:
 -- SELECT loadCEvents(1);
 
-create or replace function loadCEvents(in int, out text, out text, out text) 
+create or replace function loadCEvents(in int, out text, out text, out time, out time) 
 
 	returns setof record as
 
 $$ 
      
-	select eDate, eLocation, eTime from events
+	select eDate, eLocation, eTime_s, eTime_e from events
     
                 where e_id = $1;
     
@@ -25,11 +27,11 @@ $$
 language 'sql';
 
 --HOW TO USE:
--- SELECT setFixE(1, 'December 5, 2014', 'Iligan City', '9:00 - 5:00 pm');
+-- SELECT setFixE(1, 'December 5, 2014', 'Iligan City', '08:00 AM', '04:00 PM');
 
 create or replace function setFixE(p_e_id int,
 	p_eDate text, p_eLocation text, 
-	p_eTime text) 
+	p_eTime_s time, p_eTime_e time) 
 returns text as
 $$
 declare
@@ -39,16 +41,12 @@ begin
 	where e_id = p_e_id;
   if v_e_id isnull then
 	insert into events(e_id, eDate, 
-	eLocation, eTime) values
-	(p_e_id, p_eDate, p_eLocation, p_eTime);
+	eLocation, eTime_s, eTime_e) values
+	(p_e_id, p_eDate, p_eLocation, p_eTime_s, p_eTime_e);
   else
 	update events
 
-<<<<<<< HEAD
-	set eDate = p_eDate,eLocation = p_eLocation,eTime = p_eTime
-=======
-	set eDate = p_eDate,eDate = p_eLocation,eTime = p_eTime
->>>>>>> fbe2489c1ae4e96fff54bd5fb1934b75f77f37aa
+	set eDate = p_eDate,eLocation = p_eLocation,eTime_s = p_eTime_s,eTime_e = p_eTime_e
           where e_id = p_e_id;
 	    
   end if;
@@ -58,10 +56,10 @@ $$
     language plpgsql;
 	
 --HOW TO USE:
--- SELECT setScore(1, 20);
+-- SELECT setScore(1, 98, 100);
 
 create or replace function setScore(p_e_id int,
-	p_score int) 
+	p_scoret1 int, p_scoret2 int) 
 returns text as
 $$
 declare
@@ -70,12 +68,12 @@ begin
   select into v_e_id e_id from events
 	where e_id = p_e_id;
   if v_e_id isnull then
-	insert into events(scores) values
-	(p_score);
+	insert into events(scoret1, scoret2) values
+	(p_scoret1, p_scoret2);
   else
 	update events
 
-	set scores = p_score where e_id = p_e_id;
+	set scoret1 = p_scoret1, scoret2 = p_scoret2 where e_id = p_e_id;
 	
 	    
   end if;
@@ -87,13 +85,13 @@ $$
 --HOW TO USE:
 -- SELECT getScore(1);
 
-create or replace function getScore(in int, out int) 
+create or replace function getScore(in int, out int, out int) 
 
-	returns int as
+	returns record as
 
 $$ 
      
-	select scores from events
+	select scoret1, scoret2 from events
     
                 where e_id = $1;
     
@@ -103,11 +101,11 @@ $$
 language 'sql';
 
 --HOW TO USE:
--- SELECT ReSchedE(1, 'September 13, 2014','Cagayan','8:00 am - 4:00 pm');
+-- SELECT ReSchedE(1, 'September 13, 2014','Cagayan','06:00 AM','03:00 PM');
 
-create or replace function ReScedE(p_e_id int,
+create or replace function ReSchedE(p_e_id int,
 	p_eDate text, p_eLocation text, 
-	p_eTime text) 
+	p_eTime_s time, p_eTime_e time) 
 returns text as
 $$
 declare
@@ -117,14 +115,10 @@ begin
 	where e_id = p_e_id;
   
   update events
-<<<<<<< HEAD
-	set eDate = p_eDate, eLocation = p_eLocation,eTime = p_eTime
-=======
-	set eDate = p_eDate,eDate = p_eLocation,eTime = p_eTime
->>>>>>> fbe2489c1ae4e96fff54bd5fb1934b75f77f37aa
+	set eDate = p_eDate,eLocation = p_eLocation,eTime_s = p_eTime_s, eTime_e = p_eTime_e
           where e_id = p_e_id;
 	    
     return 'OK';
   end;
 $$
-    language plpgsql;
+    language 'plpgsql';
