17d5f20106ee12d961281c2c5cb51364198ed5a0
onnanapiando <onnapiando@gmail.com>
2014-08-14 23:01:42 +0700

    added getLeague.py to scripts folder

---
+++ scripts/getLeague.py
@@ -0,0 +1,16 @@
+from dosql import *
+import cgi
+import json
+
+def index(req, league_id):
+    league_id = cgi.escape(league_id)
+    #section = cgi.escape(sport_type)
+    #semid = cgi.escape(fixture_type)
+    x = doSql()
+    rets = x.execqry("select * from get_listing('" + league_id +  "');", False)
+    result = []
+    for ret in rets:
+        stringed = map(str, ret)
+        result.append(stringed)
+
+    return json.dumps(result)
