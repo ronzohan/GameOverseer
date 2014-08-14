6b17d22f7424afe2b47e71e92f0cac74232adf7a
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-08-14 22:44:11 +0800

    Added getTeamInfo for the stored proc get_team_per_name

---
+++ scripts/getTeamInfo.py
@@ -0,0 +1,19 @@
+from dosql import *
+import cgi
+import json
+
+
+def index(req,name):
+    name = cgi.escape(name)
+    a = doSql()
+    query = "SELECT * FROM get_team_per_name('"+ name +"');"
+    print query
+    items = a.execqry(query,False)
+    result = []
+    for item in items:
+        stringed = map(str,item)
+        result.append(stringed)
+
+    return json.dumps(result)
+
+index(1,"Chelsea")
