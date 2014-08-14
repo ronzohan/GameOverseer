edba32fb4c2cd479ffc1fb4493186933fa6f6dd9
onnanapiando <onnapiando@gmail.com>
2014-08-14 23:28:49 +0700

    modified globs.js. added the function fetchleague

--- javascript/globs.js
+++ javascript/globs.js
@@ -60,3 +60,30 @@ function fetchTeamInfo(name)
     });
 }
 
+function fetchleague(league_id)
+{
+  $.ajax({
+      url: siteloc + scriptloc + "getLeague.py",
+      data: {league_id:league_id},
+      dataType: 'json',
+      success: function (res) {
+                  console.log(res);
+                  if(res[0][0] != "None")
+                  {
+					  table = '<table border="1">';
+					  for (i = 0; i < res.length; i++)
+					  {
+						  row = res[i];
+						  table += "<tr>";
+						  for (j = 0; j < row.length; j++)
+						  {
+							  table += "<td>" + row[j] + "</td>";
+						  }
+						  table += "</tr>";
+					  }
+					  table += "</table>";
+					  $("#target").html(table); 
+				  } // end if
+              }
+    });
+}
