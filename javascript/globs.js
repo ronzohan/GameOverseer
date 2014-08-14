2d1fe8275dfb7a46689213ac6437881dbbb2f6d7
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-08-14 22:48:55 +0800

    Added fetchTeamInfo function

--- javascript/globs.js
+++ javascript/globs.js
@@ -31,3 +31,32 @@ function fetchEvent(ide)
     });
 }
 
+function fetchTeamInfo(name)
+{
+  $.ajax({
+      url: siteloc + scriptloc + "getTeamInfo.py",
+      data: {name:name
+	    },
+      dataType: 'json',
+      success: function (res) {
+                  console.log(res);
+                  if(res[0][0] != "None")
+                  {
+					  table = '<table border="1">';
+					  for (i = 0; i < res.length; i++)
+					  {
+						  row = res[i];
+						  table += "<tr>";
+						  for (j = 0; j < row.length; j++)
+						  {
+							  table += "<td>" + row[j] + "</td>";
+						  }
+						  table += "</tr>";
+					  }
+					  table += "</table>";
+					  $("#target").html(table); 
+				  } // end if
+              }
+    });
+}
+
