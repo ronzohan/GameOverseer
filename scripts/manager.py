bbb26cba3b2ac2b76be0ebb331ffe13c62e1d8ec
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-08-15 00:20:27 +0800

    Fixed indentation issue

--- scripts/manager.py
+++ scripts/manager.py
@@ -4,8 +4,8 @@ import json
 
 def index(req,manager_id):
     manager_id = cgi.escape(manager_id)
-     x = doSql()
-   rets = x.execqry("select * from get_manager_per_id('" + manager_id + "');", False)
+    x = doSql()
+    rets = x.execqry("select * from get_manager_per_id('" + manager_id + "');", False)
     result = []
     for ret in rets:
         stringed = map(str, ret)
