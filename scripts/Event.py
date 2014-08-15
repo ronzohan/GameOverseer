a464a26ece4734453474744114b7455f4cf053f3
Kay Apao <kayapao@yahoo.com>
2014-08-15 09:13:22 +0800

    changing the wrongly used stored proc in Event.py

--- scripts/Event.py
+++ scripts/Event.py
@@ -5,7 +5,7 @@ import json
 def index(req, ide):
     ide = cgi.escape(ide)
     x = doSql()
-    rets = x.execqry("select * from get_listing('" + ide + "');", False)
+    rets = x.execqry("select * from loadCEvents('" + ide + "');", False)
     result = []
     for ret in rets:
         stringed = map(str, ret)
