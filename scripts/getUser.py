4ca6066f5e844b1dc254d9ced44681c1ac8432b5
supersherlock <diofel.pallega@gmail.com>
2014-08-16 13:03:36 +0800

    getUser updated, corrected id to userid.

---
+++ scripts/getUser.py
@@ -0,0 +1,16 @@
+from dosql import *
+import cgi
+import simplejson as json
+
+def index(req, userid):
+    id = cgi.escape(userid) 
+    x = doSql()
+    rets = x.execqry("select * from get_users_perid('" + id + "');", False)
+    result = []
+    for ret in rets:
+        stringed = map(str, ret)
+        result.append(stringed)
+
+    return json.dumps(result)
+
+	 
