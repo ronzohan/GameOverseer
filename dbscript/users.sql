63ce70107fce2335d5b4a9dbbc83eb66ab376124
supersherlock <diofel.pallega@gmail.com>
2014-08-16 15:06:26 +0800

    Added address of the user, and the contactno of the user. Added change address function on users.sql'

--- dbscript/users.sql
+++ dbscript/users.sql
@@ -3,9 +3,44 @@ CREATE EXTENSION pgcrypto;
 CREATE TABLE users(
 	userid int primary key,
 	username text,
-	password text
+	password text,
+	address text,
+	contactno text
 );
 
+ 
+create or replace function
+   changeUserAddress(p_username text, p_password text,p_address text)
+     returns text as
+$$
+   declare
+      originalpassword1 text;
+      userpassword1 text;
+	  contactno1 text;
+	  userid1 int;
+	  
+   begin
+      select into userpassword1 password from users where username = p_username; 
+	  select into contactno1 contactno from users where username = p_username;
+      select into userid1 userid contactno from users where username = p_username;  	  
+	  
+	   originalpassword1 = userpassword1;
+	   userpassword1 = crypt(p_password,userpassword1);
+      if originalpassword1 = userpassword1 then
+          update users
+			set   address = p_address
+			--this gen_salt generates a new random salt string
+			-- crypt() does the hashing
+			where username = p_username;
+     else
+         return 'Your password did not match';
+      end if;
+	  return 'change successful';
+  end;
+$$
+language 'plpgsql';
+-- HOW TO USE :
+-- SELECT changeUserAddress(p_username text, p_password text,p_address text); 
 
 create or replace function 
    login(p_usernameinput text, p_passwordinput text)
@@ -40,21 +75,22 @@ $$
 
 
 create or replace function
-   setuser(p_userid int,p_username text, p_password text)
+   setuser(p_userid int,p_username text, p_password text,p_address text,p_contactno text)
      returns text as
 $$
    declare
+      originalpassword text;
       userid1 int;
    begin
       select into userid1 userid from users
          where userid = p_userid;
 	  
       if userid1 isnull then
-         insert into users(userid,username,password) values
-            (p_userid,p_username,crypt(p_password, gen_salt('bf')));
+         insert into users(userid,username,password,address,contactno) values
+            (p_userid,p_username,crypt(p_password, gen_salt('bf')),p_address,p_contactno);
      else
           update users
-			set userid = p_userid,username = p_username, password = crypt(p_password, gen_salt('bf')) 
+			set userid = p_userid,username = p_username, password = crypt(p_password, gen_salt('bf')),address = p_addres, contactno = p_contactno
 			--this gen_salt generates a new random salt string
 			-- crypt() does the hashing
 			where username = p_username;
@@ -68,14 +104,14 @@ language 'plpgsql';
 
 --view
 create or replace function
-   get_users_perid(in int, out int, out text)
+   get_users_perid(in int, out int, out text,out text,out text)
 returns setof record as
 $$
-	select userid,username from users
+	select userid,username,address,contactno from users
 	where userid = $1;
 
 $$
   language 'sql';
 
 -- HOW TO USE:
--- select * from get_user_perid(userid)
+-- SELECT get_users_perid(userid);
