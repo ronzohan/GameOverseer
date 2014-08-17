ed76ebac6b0b6e0dc580b0e9c226178e023ee2f5
supersherlock <diofel.pallega@gmail.com>
2014-08-17 09:46:10 +0800

    removed pointless fetch function in globs.js

--- javascript/globs.js
+++ javascript/globs.js
@@ -32,36 +32,6 @@ function fetchEvent(ide)
     });
 }
 
-
-function fetchUser(userid)
-{
-  $.ajax({
-      url: siteloc + scriptloc + "getUser.py",
-      data: {userid:userid
-	    },
-      dataType: 'json',
-      success: function (res) {
-                  console.log(res);
-                  if(res[0][0] != "None")
-                  {
-					  table = '<table border="1">';
-					  for (i = 0; i < res.length; i++)
-					  {
-						  row = res[i];
-						  table += "<tr>";
-						  for (j = 0; j < row.length; j++)
-						  {
-							  table += "<td>" + row[j] + "</td>";
-						  }
-						  table += "</tr>";
-					  }
-					  table += "</table>";
-					  $("#target").html(table); 
-				  } // end if
-              }
-    });
-}
-
 function fetchTeamInfo(name)
 {
   $.ajax({
