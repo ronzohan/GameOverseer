9e8aa1b5faa576516a099a45e34476517122ad3a
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-08-17 11:16:00 +0800

    Reverted globs.js and added back fetchUser due to wrong instruction given

--- javascript/globs.js
+++ javascript/globs.js
@@ -2,12 +2,43 @@
 var siteloc = "http://localhost/GameOverseer";
 var scriptloc = "/scripts/"
 
+ 
 function fetchEvent(ide)
 {
   $.ajax({
       url: siteloc + scriptloc + "Event.py",
       data: {ide:ide,
 				},
+      dataType: 'json',
+      success: function (res) {
+                  console.log(res);
+                  if(res[0][0] != "None")
+                  {
+					  table = '<table border="1">';
+					  for (i = 0; i < res.length; i++)
+					  {
+						  row = res[i];
+						  table += "<tr>";
+						  for (j = 0; j < row.length; j++)
+						  {
+							  table += "<td>" + row[j] + "</td>";
+						  }
+						  table += "</tr>";
+					  }
+					  table += "</table>";
+					  $("#target").html(table); 
+				  } // end if
+              }
+    });
+}
+
+
+function fetchUser(userid)
+{
+  $.ajax({
+      url: siteloc + scriptloc + "getUser.py",
+      data: {userid:userid
+	    },
       dataType: 'json',
       success: function (res) {
                   console.log(res);
