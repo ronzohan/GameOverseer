8dc8c214a93e74bf3244441d94f5b83502ad037f
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-08-28 14:21:04 +0800

    Changed primary key to serial, added foreign key, updated stored proc

--- dbscript/league.sql
+++ dbscript/league.sql
@@ -1,6 +1,7 @@
 create table league (
-     league_id int primary key,
-     league_name text,
+     league_id serial primary key,
+     managerid_fk serial references manager (managerid),
+     name text,
      sport_type text,
      fixture_type text
 );
@@ -8,22 +9,22 @@ create table league (
 --create language plpgsql;
 -- controller
 create or replace 
-    function setleague(p_league_id int, p_league_name text,p_sport_type text, p_fixture_type text) 
+    function setleague(p_managerid_fk int,p_name text,p_sport_type text, p_fixture_type text) 
     returns text as
 $$
   declare
-     v_league_id int;
+     v_name text;
   begin
-      select into v_league_id league_id from league 
-         where league_id = p_league_id;
+      select into v_name name from league 
+         where managerid_fk = p_managerid_fk and name = p_name and sport_type = p_sport_type and fixture_type = p_fixture_type;
          
-      if v_league_id isnull then
-          insert into league(league_id, league_name, sport_type,fixture_type) values
-             (p_league_id, p_league_name, p_sport_type,p_fixture_type);
+      if v_name isnull then
+          insert into league(managerid_fk,name, sport_type,fixture_type) values
+             (p_managerid_fk, p_name, p_sport_type,p_fixture_type);
       else
            update League
-           set league_name = p_league_name,sport_type = p_sport_type,fixture_type = p_fixture_type
-             where league_id = p_league_id;
+           set name = p_name,sport_type = p_sport_type,fixture_type = p_fixture_type
+             where managerid_fk = p_managerid_fk;
 
       end if;   
          
@@ -41,7 +42,7 @@ create or replace function
 returns setof record as
 $$ 
 
-     select  league_name, sport_type,fixture_type from league
+     select  name, sport_type,fixture_type from league
      where league_id = $1;
      
 $$
