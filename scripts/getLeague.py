d592b7b521df1401797692be158e19c476cb6fc7
onnanapiando <onnapiando@gmail.com>
2014-09-01 12:48:09 +0700

    deleted unnecessary comments

--- scripts/getLeague.py
+++ scripts/getLeague.py
@@ -7,8 +7,7 @@ except ImportError:
 
 def index(req, league_id):
     league_id = cgi.escape(league_id)
-    #section = cgi.escape(sport_type)
-    #semid = cgi.escape(fixture_type)
+
     x = doSql()
     rets = x.execqry("select * from get_league_perid('" + league_id +  "');", False)
     result = []
