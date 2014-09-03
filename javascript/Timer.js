cd1e37cb5fa81f62fa2c875ee8f41501fa73b338
Kay Apao <kayapao@yahoo.com>
2014-09-03 21:41:15 +0800

    addition of addTimer function to Timer.js, change of url

--- javascript/Timer.js
+++ javascript/Timer.js
@@ -1,90 +1,62 @@
-/*
-Author: Robert Hashemian
-http://www.hashemian.com/
+var TotalSeconds;
+var Seconds;
+var Hours;
+var Days;
+var Minutes;
+var TimeStr;
+var TimerID;
 
-You can use this code in any manner so long as the author's
-name, Web address and this disclaimer is kept intact.
-********************************************************
-Usage Sample:
-
-<script language="JavaScript">
-TargetDate = "12/31/2020 5:00 AM";
-BackColor = "palegreen";
-ForeColor = "navy";
-CountActive = true;
-CountStepper = -1;
-LeadingZero = true;
-DisplayFormat = "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
-FinishMessage = "It is finally here!";
-</script>
-<script language="JavaScript" src="http://scripts.hashemian.com/js/countdown.js"></script>
-*/
-
-function calcage(secs, num1, num2) {
-  s = ((Math.floor(secs/num1))%num2).toString();
-  return "<b>" + s + "</b>";
+function addTimer(TDate, FMessage, TimID){
+	FinishMessage = FMessage;
+	TimerID = TimID;
+	var TargetDate = TDate;
+	var dtarg = new Date(TargetDate);
+	var dnow = new Date();
+	diff = new Date(dtarg - dnow);
+	if(diff < 0){
+		Timer.innerHTML = FinishMessage;
+	}
+	Time = Math.floor(diff.valueOf()/1000);
+	CreateTimer(TimerID, Time);
+	UpdateTimer();
 }
 
-function CountBack(secs) {
-  if (secs < 0) {
-    document.getElementById("cntdwn").innerHTML = FinishMessage;
-    return;
-  }
-  if (calcage(secs,86400,100000) == "<b>0</b>") 
-    DisplayFormat = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp %%H%% hr(s) %%M%% min(s)";
-  else{
-	DisplayFormat = "&nbsp;&nbsp;&nbsp;&nbsp %%D%% day(s)"
-	DisplayFormat = DisplayFormat.replace(/%%D%%/g, calcage(secs,86400,100000));
-  }
-  if (calcage(secs,3600,24) == "<b>0</b>") 
-    DisplayStr = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp %%M%% m";
-  else
-	DisplayStr = DisplayFormat.replace(/%%H%%/g, calcage(secs,3600,24));
-	
-  DisplayStr = DisplayStr.replace(/%%M%%/g, calcage(secs,60,60));
-  DisplayStr = DisplayStr.replace(/%%S%%/g, calcage(secs,1,60));
+function CreateTimer(TimerID, Time) {
+	Timer = document.getElementById(TimerID);
+	TotalSeconds = Time;
 
-  document.getElementById("cntdwn").innerHTML = DisplayStr;
-  if (CountActive)
-    setTimeout("CountBack(" + (secs+CountStepper) + ")", SetTimeOutPeriod);
+	UpdateTimer();
+	window.setTimeout("Tick()", 1000);
 }
 
-function putspan(backcolor, forecolor) {
- document.write("<span id='cntdwn' style='background-color:" + backcolor + 
-                "; color:" + forecolor + "'></span>");
+function Tick() {
+	if (TotalSeconds <= 0) {
+		Timer.innerHTML = FinishMessage;
+		return;
+	}
+
+	TotalSeconds -= 1;
+	UpdateTimer();
+	window.setTimeout("Tick()", 1000);
+}
+ 
+function UpdateTimer() {
+	Seconds = TotalSeconds;
+
+	Days = Math.floor(Seconds / 86400);
+	Seconds -= Days * (86400);
+
+	Hours = Math.floor(Seconds / 3600);
+	Seconds -= Hours * (3600);
+
+	Minutes = Math.floor(Seconds / 60);
+	Seconds -= Minutes * (60);
+
+	TimeStr = ((Days > 0) ? Days + " days " : "") + (((Hours > 0) && (Days <= 0)) ? Hours + " h " : "") + (((Minutes > 0) && (Days <= 0)) ? Minutes + " m " : "") + (((Seconds > 0) && (Days <= 0) && (Hours <= 0) && (Minutes <= 0)) ? Seconds + " s " : "");
+
+	Timer.innerHTML = TimeStr;
 }
 
 
+addTimer(TargetDate, FinishMessage, TimerID);
 
-if (typeof(BackColor)=="undefined")
-  BackColor = "transparent";
-if (typeof(ForeColor)=="undefined")
-  ForeColor= "white";
-if (typeof(TargetDate)=="undefined")
-  TargetDate = "12/31/2014 5:00 AM";
-if (typeof(DisplayFormat)=="undefined")
-  DisplayFormat = "%%D%% d %%H%% h %%M%% m";
-if (typeof(CountActive)=="undefined")
-  CountActive = true;
-if (typeof(FinishMessage)=="undefined")
-  FinishMessage = "Currently Playing";
-if (typeof(CountStepper)!="number")
-  CountStepper = -1;
-if (typeof(LeadingZero)=="undefined")
-  LeadingZero = true;
-
-
-CountStepper = Math.ceil(CountStepper);
-if (CountStepper == 0)
-  CountActive = false;
-var SetTimeOutPeriod = (Math.abs(CountStepper)-1)*1000 + 990;
-putspan(BackColor, ForeColor);
-var dthen = new Date(TargetDate);
-var dnow = new Date();
-if(CountStepper>0)
-  ddiff = new Date(dnow-dthen);
-else
-  ddiff = new Date(dthen-dnow);
-
-gsecs = Math.floor(ddiff.valueOf()/1000);
-CountBack(gsecs);
