0434a7d5e5722fce688e25a73be2ad21da1f93f7
Kay Apao <kayapao@yahoo.com>
2014-09-03 21:47:33 +0800

    deletion of unnecessary lines

--- javascript/Timer.js
+++ javascript/Timer.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-=======
 var Timer;
->>>>>>> ce8f70fd9aa1383c532a020a7792bf24605d7266
 var TotalSeconds;
 var Seconds;
 var Hours;
 var Days;
 var Minutes;
 var TimeStr;
-<<<<<<< HEAD
 var TimerID;
 
 function addTimer(TDate, FMessage, TimID){
@@ -25,9 +21,6 @@ function addTimer(TDate, FMessage, TimID){
 	CreateTimer(TimerID, Time);
 	UpdateTimer();
 }
-=======
-
->>>>>>> ce8f70fd9aa1383c532a020a7792bf24605d7266
 
 function CreateTimer(TimerID, Time) {
 	Timer = document.getElementById(TimerID);
@@ -48,7 +41,6 @@ function Tick() {
 	window.setTimeout("Tick()", 1000);
 }
  
-<<<<<<< HEAD
 function UpdateTimer() {
 	Seconds = TotalSeconds;
 
@@ -65,7 +57,6 @@ function UpdateTimer() {
 
 	Timer.innerHTML = TimeStr;
 }
-=======
 
 function UpdateTimer() {
 	Seconds = TotalSeconds;
@@ -78,28 +69,13 @@ function UpdateTimer() {
 
 	Minutes = Math.floor(Seconds / 60);
 	Seconds -= Minutes * (60);
->>>>>>> ce8f70fd9aa1383c532a020a7792bf24605d7266
 
 	TimeStr = ((Days > 0) ? Days + " days " : "") + (((Hours > 0) && (Days <= 0)) ? Hours + " h " : "") + (((Minutes > 0) && (Days <= 0)) ? Minutes + " m " : "") + (((Seconds > 0) && (Days <= 0) && (Hours <= 0) && (Minutes <= 0)) ? Seconds + " s " : "");
 
-<<<<<<< HEAD
+
+	Timer.innerHTML = TimeStr;
+}
+
 addTimer(TargetDate, FinishMessage, TimerID);
 
-=======
-	Timer.innerHTML = TimeStr;
-}
 
-
-FinishMessage = "Live";
-TimerID = "timer";
-var TargetDate = "9/3/2014 2:01 PM";
-var dtarg = new Date(TargetDate);
-var dnow = new Date();
-diff = new Date(dtarg - dnow);
-if(diff < 0){
-	diff = diff * -1;
-}
-Time = Math.floor(diff.valueOf()/1000);
-CreateTimer(TimerID, Time);
-UpdateTimer();
->>>>>>> ce8f70fd9aa1383c532a020a7792bf24605d7266
