6b7a43bb01b22b4fccbfd2674b13592c9cf32434
Kay Apao <kayapao@yahoo.com>
2014-09-06 11:21:37 +0800

    error in timer corrected

--- javascript/Timer.js
+++ javascript/Timer.js
@@ -1,81 +1,38 @@
 var Timer;
 var TotalSeconds;
-var Seconds;
-var Hours;
-var Days;
-var Minutes;
-var TimeStr;
-var TimerID;
 
-function addTimer(TDate, FMessage, TimID){
-	FinishMessage = FMessage;
-	TimerID = TimID;
-	var TargetDate = TDate;
-	var dtarg = new Date(TargetDate);
-	var dnow = new Date();
-	diff = new Date(dtarg - dnow);
-	if(diff < 0){
-		Timer.innerHTML = FinishMessage;
-	}
-	Time = Math.floor(diff.valueOf()/1000);
-	CreateTimer(TimerID, Time);
-	UpdateTimer();
+function CreateTimer(TimerID, Time){
+    var oop=this;
+	this.Timer = document.getElementById(TimerID);
+	this.TotalSeconds = Time;
+	this.update();
+	oop.to=setTimeout(function(){ oop.tick(); }, 1000);
 }
 
-function CreateTimer(TimerID, Time) {
-	Timer = document.getElementById(TimerID);
-	TotalSeconds = Time;
+CreateTimer.prototype={
 
-	UpdateTimer();
-	window.setTimeout("Tick()", 1000);
-}
-
-function Tick() {
-	if (TotalSeconds <= 0) {
-		Timer.innerHTML = FinishMessage;
+ tick:function(){
+    var oop=this;
+	if (this.TotalSeconds <= 0){
+		this.Timer.innerHTML = FinishMessage;
 		return;
 	}
+	this.TotalSeconds -= 1;
+	this.update()
+	oop.to=setTimeout(function(){ oop.tick(); }, 1000);
+ },
 
-	TotalSeconds -= 1;
-	UpdateTimer();
-	window.setTimeout("Tick()", 1000);
-}
+ update:function(){
+ 	var Seconds = this.TotalSeconds,Days = Math.floor(Seconds / 86400);
+	Seconds -= Days * 86400;
+	var Hours = Math.floor(Seconds / 3600);
+	Seconds -= Hours * (3600);
+	var Minutes = Math.floor(Seconds / 60);
+	Seconds -= Minutes * (60);
+	var TimeStr = ((Days > 0) ? Days + " days " : "") + (((Hours > 0) && (Days <= 0)) ? Hours + " h " : "") + (((Minutes > 0) && (Days <= 0)) ? Minutes + " m " : "") + (((Seconds > 0) && (Days <= 0) && (Hours <= 0) && (Minutes <= 0)) ? Seconds + " s " : "");
+	this.Timer.innerHTML = TimeStr;
+ }
  
-function UpdateTimer() {
-	Seconds = TotalSeconds;
 
-	Days = Math.floor(Seconds / 86400);
-	Seconds -= Days * (86400);
-
-	Hours = Math.floor(Seconds / 3600);
-	Seconds -= Hours * (3600);
-
-	Minutes = Math.floor(Seconds / 60);
-	Seconds -= Minutes * (60);
-
-	TimeStr = ((Days > 0) ? Days + " days " : "") + (((Hours > 0) && (Days <= 0)) ? Hours + " h " : "") + (((Minutes > 0) && (Days <= 0)) ? Minutes + " m " : "") + (((Seconds > 0) && (Days <= 0) && (Hours <= 0) && (Minutes <= 0)) ? Seconds + " s " : "");
-
-	Timer.innerHTML = TimeStr;
 }
-
-function UpdateTimer() {
-	Seconds = TotalSeconds;
-
-	Days = Math.floor(Seconds / 86400);
-	Seconds -= Days * (86400);
-
-	Hours = Math.floor(Seconds / 3600);
-	Seconds -= Hours * (3600);
-
-	Minutes = Math.floor(Seconds / 60);
-	Seconds -= Minutes * (60);
-
-	TimeStr = ((Days > 0) ? Days + " days " : "") + (((Hours > 0) && (Days <= 0)) ? Hours + " h " : "") + (((Minutes > 0) && (Days <= 0)) ? Minutes + " m " : "") + (((Seconds > 0) && (Days <= 0) && (Hours <= 0) && (Minutes <= 0)) ? Seconds + " s " : "");
-
-
-	Timer.innerHTML = TimeStr;
-}
-
-addTimer(TargetDate, FinishMessage, TimerID);
-
-
+	
