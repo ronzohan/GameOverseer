b5ab3ad740d8721d5df3de4eac7b34f4932052cb
onnanapiando <onnapiando@gmail.com>
2014-09-08 14:58:12 +0700

    used whitespace; don't have overly long lines

--- scripts/getLeague.py
+++ scripts/getLeague.py
@@ -9,19 +9,22 @@ def index(req, league_id):
     league_id = cgi.escape(league_id)
 
     x = doSql()
-    rets = x.execqry("select * from get_league_perid('" + league_id +  "');", False)
+    rets = x.execqry("select * from get_league_perid('" + \
+    league_id +  "');", False)
     result = []
+    
     for ret in rets:
         stringed = map(str, ret)
         result.append(stringed)
-
-    return json.dumps(result)
+	return json.dumps(result)
+	
 
 def getBracketInfo(req,league_id):
     league_id = cgi.escape(league_id)
 
     x = doSql()
-    rets = x.execqry("select * from get_league_bracket_info('" + league_id +  "');", False)
+    rets = x.execqry("select * from get_league_bracket_info('" \
+    + league_id +  "');", False)
     ##doesnt need to map rets to str  since the needed data is int[]
     return json.dumps(rets)
 
@@ -29,21 +32,25 @@ def getLeagueInfoByManager(req,managerid):
     managerid = cgi.escape(managerid)
 
     x = doSql()
-    rets = x.execqry("select * from get_league_by_manager('" + managerid +  "');", False)
+    rets = x.execqry("select * from get_league_by_manager('" + \
+    managerid +  "');", False)
     result = []
+    
     for ret in rets:
         stringed = map(str, ret)
         result.append(stringed)
-
-    return json.dumps(result)
+	return json.dumps(result)
+	
 
 def setleague(req,managerid,leaguename,fixturetype,sport):
     leaguename = cgi.escape(leaguename)
     fixturetype = cgi.escape(fixturetype)
     sport = cgi.escape(sport)
     x = doSql()
-    rets = x.execqry("select * from setleague('" + managerid +  "','"+leaguename+"','"+sport+"','"+fixturetype+"');", True)
+    rets = x.execqry("select * from setleague('" + managerid +  "' ," \
+    " '"+leaguename+"','"+sport+"','"+fixturetype+"');", True)
     result = []
+    
     for ret in rets:
         stringed = map(str, ret)
         result.append(stringed)
