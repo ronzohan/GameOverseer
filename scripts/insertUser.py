8a7dce8761a1c5fc63b2ad849f5776ea9f104138
supersherlock <diofel.pallega@gmail.com>
2014-09-08 19:44:11 +0800

    added inputs in index

--- scripts/insertUser.py
+++ scripts/insertUser.py
@@ -5,16 +5,23 @@ try:
 except ImportError:
     import simplejson as json
 
-def index(req, username,password):
-    username= cgi.escape(username)   
-    password = cgi.escape(password) 
-    x = doSql() 
-    x.execqry("select  setuser('" + username+" ',' "+ password+" ');", False)
+
+def index(req,username,password,fullname,email,contactno,address):
+    username = cgi.escape(username)
+    password = cgi.escape(password)
+	fullname = cgi.escape(fullname)
+    email = cgi.escape(email)
+	contactno = cgi.escape(contactno)
+    address = cgi.escape(address)
+	
+    a = doSql()
+    query = "SELECT * FROM setuser('"+ username+"','"+ password+"','"+fullname+"','"+email+"','"+contactno+"','"+address+"');"
+    print query
+    items = a.execqry(query,False)
     result = []
-    for ret in rets:
-        stringed = map(str, ret)
+    for item in items:
+        stringed = map(str,item)
         result.append(stringed)
-		
+
     return json.dumps(result)
 
-	  
