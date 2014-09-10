1f00b9e566bcde1f4f1816e6bf5f470f9ad6a659
Jennie Ramos <jennieramos06@gmail.com>
2014-09-09 23:49:24 -0700

    add function to users

--- dbscript/users.sql
+++ dbscript/users.sql
@@ -87,3 +87,16 @@ $$
 
 -- HOW TO USE:
 -- select * from get_users_perid(userid)
+
+create or replace function
+   get_username(in text, out text)
+returns text as
+$$
+	select username from users
+	where username = $1;
+    
+$$
+  language 'sql';
+
+-- HOW TO USE:
+-- select * from get_username(username)
