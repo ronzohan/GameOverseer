fbae71a14cf5f22bd2e2571e0b62f848427eb9af
onnanapiando <onnapiando@gmail.com>
2014-09-12 22:53:12 +0700

    edited fetchmanager

--- javascript/globs.js
+++ javascript/globs.js
@@ -119,8 +119,12 @@ function fetchmanager(manager_id)
                   console.log(res);
                   if(res[0][0] != "None")
                   {
-					   
-				  		}
+					   $("#fullname1").append(res);
+					   $("#username1").append(res);
+					   $("#emailadd1").append(res);
+					   $("#address1").append(res);
+					   $("#phone1").append(res);
+				  }
               }
     });
 }
