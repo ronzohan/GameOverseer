2bc6bf8b01504bc20ea04f0410e30964b5037b2b
supersherlock <diofel.pallega@gmail.com>
2014-09-13 20:20:18 +0800

    line 162 , href leagueinfo -> leagueinfo.html

--- javascript/globs.js
+++ javascript/globs.js
@@ -159,7 +159,7 @@ function fetchLeagueByManagerId(managerid)
 			{
 				row = res[i];
 				     
-				$("#leaguetable").append('<tr><td><a href=leagueinfo?id='+row[0]+'>'+row[1]+'</a></td>'
+					$("#leaguetable").append('<tr><td><a href=leagueinfo.html?id='+row[0]+'>'+row[1]+'</a></td>'
 		      	  		+ '<td>'+row[2]+'</td>' + '<td>'+row[3]
 					+'</td><td><a href="#" onClick=editLeague('+getCookie('userid')+','+row[0]+') class="glyphicon glyphicon-pencil">Edit</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'
 		      	  		+'<a href="#" onClick = verifydelete('+row[0]+','+getCookie("userid")+') class="glyphicon glyphicon-remove">Remove</a></td></tr>');
