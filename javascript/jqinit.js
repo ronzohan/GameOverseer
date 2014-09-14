cc01e91eacfed225b2404d481660c5ce715fb27e
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-09-15 01:25:50 +0800

    Placed ondocument ready function redirect_ifNotLoggedin(), bind function on confirmteam button click

--- javascript/jqinit.js
+++ javascript/jqinit.js
@@ -1,10 +1,20 @@
 $(function() 
 {
-	if (isloggedin())
-		$("#header").load("header.html");
-  	else 
-		window.location.replace("login.html");
- 
+	redirect_ifNotloggedin();
+	
+	fetchLeagueBracketInfo(getParameterByName('id'))
+	//parameter depends on who's the user, used on leagueinfo.html
+	
+	$("#confirmteams").click(function(){
+		var participantTeams = []; 
+		$('#teamcollection > tbody > tr').each(function(i,row) {
+			participantTeams.push(row.cells[0].textContent);
+		});
+		participantTeams = JSON.stringify(participantTeams, null, 2);
+		console.log(participantTeams);	
+		confirmAddTeamsInLeague(getParameterByName('id'),getCookie('userid'),participantTeams);
+	});
+	
 	$("#newleague").click(
 		function () {
 			$("#create").show();
@@ -19,7 +29,11 @@ $(function()
 			$("#addteamdialog").dialog('open');
 		}
     );
- 
 	
- 
+		
+	$(document).on("click", "a.removeteams", function(){
+		 $(this).parent().parent().remove();
+	});
+	
+	
 }); 
