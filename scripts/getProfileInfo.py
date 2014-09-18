2488ba95fae397ab2b7b5c0ec0a05a6dbbd52708
Kay Apao <kayapao@yahoo.com>
2014-09-18 21:20:15 +0800

    deletion of unnecessary parameters being called

--- scripts/getProfileInfo.py
+++ scripts/getProfileInfo.py
@@ -9,7 +9,7 @@ def index(req, userid, userid_fk):
     id = cgi.escape(userid) 
     id2 = cgi.escape(userid_fk)
     x = doSql()
-    rets = x.execqry("SELECT users.userid, users.username, userid_fk, \
+    rets = x.execqry("SELECT users.username, \
     first_name, last_name, address, contact_no, email FROM users \
     INNER JOIN manager ON users.userid = manager.userid_fk;", False)
     result = []
