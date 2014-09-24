43c700071f1537e210e4a5855f79b8c2f30b3bb0
onnanapiando <onnapiando@gmail.com>
2014-09-24 23:33:27 +0700

    added new python file

---
+++ scripts/updatemanager.py
@@ -0,0 +1,43 @@
+from dosql import *
+import cgi
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+
+def index(req,username,password,firstname,lastname,email,contactno,address):
+    username = cgi.escape(username)
+    password = cgi.escape(password)
+    firstname= cgi.escape(firstname)
+    lastname = cgi.escape(lastname)
+    email = cgi.escape(email)
+    contactno = cgi.escape(contactno)
+    address = cgi.escape(address)
+     
+ 
+ 
+    a = doSql()
+  
+   
+ 
+    query = "SELECT login('"+ username+"','"+ password+"');" 
+    items = b.execqry(query,False)
+    for item in items:
+        stringed = map(int,item)
+        [a] =  stringed
+
+    id = str(a)
+ 
+   
+ 
+       
+  
+    query = "SELECT setmanager("+id+" ,'"+firstname+"','"+lastname+"','"+address+"','"+contactno+"','"+email+"');"
+    items = b.execqry(query,True)
+    result2 = []
+    for item in items:
+        stringed = map(str,item)
+        result2.append(stringed)
+
+    return json.dumps(result2)
