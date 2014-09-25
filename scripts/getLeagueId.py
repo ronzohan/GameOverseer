620659ef2774eb6cd4f425316b29d5f4e09f19c2
Kay Apao <kayapao@yahoo.com>
2014-09-25 19:57:00 +0800

    added new file getLeagueId.py

---
+++ scripts/getLeagueId.py
@@ -0,0 +1,17 @@
+from dosql import *
+import cgi
+try:
+    import json
+except ImportError:
+    import simplejson as json
+	
+def getleagueID(req,name):
+    name= cgi.escape(name)    
+    x = doSql() 
+    rets = x.execqry("select league_id from league where name = '" + name + "';",False)
+    result = []
+    for ret in rets:
+        stringed = map(str, ret)
+        result.append(stringed)
+		
+    return json.dumps(result)  
