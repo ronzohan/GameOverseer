118dcf6740c3c6ea2a374cdb912fe615565aa1e9
Jennie Ramos <jennieramos06@gmail.com>
2014-10-01 09:48:39 +0800

    edit name of column

--- dbscript/manager.sql
+++ dbscript/manager.sql
@@ -7,10 +7,36 @@ CREATE TABLE Manager(
     contact_no text,
 	email text,
     temManager text,
-	authority text
+	gatePass text
     );
 
 	
+create or replace 
+    function setAuthority(p_userid int,p_ text,p_last_name text,p_address text, p_contact_no text,p_email text) 
+    returns text as
+$$
+  declare
+     v_manager_id int;
+  begin
+      select into  v_manager_id manager_id from Manager 
+         where userid_fk = p_userid;
+         
+      if v_manager_id isnull then
+          insert into Manager(userid_fk,first_name,last_name,address,contact_no,email) values
+             (p_userid,p_first_name,p_last_name, p_address, p_contact_no, p_email);
+      else
+          update Manager
+          set first_name = p_first_name,last_name = p_last_name,address = p_address,contact_no = p_contact_no, email = p_email
+          where userid_fk = p_userid;
+      end if;   
+         
+      return 'OK';
+  end;
+  $$
+  language 'plpgsql';
+
+--SELECT setmanager(1, 'ron','magno','iligan city',1234,ron@yolo.com);
+
 	
 create or replace 
     function checkemail(p_email text) 
