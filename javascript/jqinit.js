1d4c131850d503719e3262eba537b1a0a8084afe
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-10-09 00:11:59 +0800

    Changed binding of buttons because of jquery dialog to  bootstrap modal replacement

--- javascript/jqinit.js
+++ javascript/jqinit.js
@@ -17,25 +17,47 @@ $(function()
 		function () {
 			$("#create").show();
 			$("#status").empty();
-            $("#adddialog").dialog('open');
-            return false;
+			$("input").prop('disabled', false); //disable all inputs since league was been successfully created
+			$("input[type=text]").val('');
+            $("#newleagueModal").modal({backdrop:'static'});
+             
         }
     );
         
+    $("#addleague").click(
+		function () {
+			$("#create").show();
+			$("#status").empty();
+            setleague($.cookie('managerid'),$('#leaguename').val(),
+			$('#fixturetype').val(),$('#sport').val());	
+            fetchLeagueByManagerId($.cookie('managerid'));
+        }
+    );
+
     $("#addteams").click(
 		function(){
-			$("#addteamdialog").dialog('open');
+			$("#addteammodal").modal(); 
 			searchAutocomplete();
 		}
     );
+    $("#addteaminleague").click(function(){
+    	var teamname = $('#teamname').val(); 							
+		addTeamsInLeague(getParameterByName('id'),$.cookie('managerid'),teamname);
+		
+	    });
 	
 	$("#lockteams").click(
 		function(){
-			$("#confirm-dialog").dialog('open');
+			$("#confirmmodal").modal();
 		}
 	
 	);
-		
+	
+	$("#confirmteamsmodal").click(function(){
+		lockTeams($.cookie('userid'),getParameterByName('id'),$.cookie('managerid'));
+		getBracketInfo(getParameterByName('id'),fetchLeagueBracketInfo);
+		$("#confirmmodal").modal('hide');
+	});	
 	$(document).on("click", "a.removeteams", function(){
 		 $(this).parent().parent().remove();
 	});
