a451623b41d6328cb55ff1a534a4592367c2db2b
Kay Apao <kayapao@yahoo.com>
2014-10-11 18:01:12 +0800

    added another query (checkTempMan.py)

--- scripts/checkTempMan.py
+++ scripts/checkTempMan.py
@@ -26,18 +26,25 @@ def index(req, id):
         stringed = map(str,ret)
         result.append(stringed)
     
-    rets = x.execqry("SELECT name from league INNER JOIN manager on league.managerid_fk = manager.manager_id \
-    where tempManager=" + id + ";" , False)	
-	
+    rets = x.execqry("select tempManLeague from manager where tempManager=" + id + ";", False);
     
+    tmp = []
     for ret in rets:
-        stringed = ''.join(map(str,ret))
-        temp.append(stringed)
-    result.append(temp)	
-
+        stringed = ''.join(map(str, ret))
+        tmp.append(stringed)
+    
+    for tmps in tmp:
+        rets = x.execqry("select name from league where league_id='" + tmps +"';", False);
+        	
+        for ret in rets:
+            stringed = ''.join(map(str, ret))
+            temp.append(stringed)
+    			
+    result.append(temp)
+    	
     rets = x.execqry("SELECT username from users \
     INNER JOIN manager ON users.userid=manager.userid_fk \
-    where tempManager=" + id + ";" , False)
+    where tempManager='" + id + "';" , False)
 	
     temp = []
     for ret in rets:
