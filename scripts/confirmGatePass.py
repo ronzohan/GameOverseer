209c65082322d8c307e18a33690a08bca03c8d40
Kay Apao <kayapao@yahoo.com>
2014-10-11 19:51:50 +0800

    added if statement and removed unnecessary comment (confirmGatePass.py)

--- scripts/confirmGatePass.py
+++ scripts/confirmGatePass.py
@@ -6,27 +6,32 @@ except ImportError:
     import simplejson as json
 
 
-def index(req, managerid, password):
+def index(req, managerid, password, league):
     managerid = cgi.escape(managerid)
     password = cgi.escape(password)
-	
+    league = cgi.escape(league)	
+    
     a =  doSql()
 	
-    rets = a.execqry("SELECT gatepass FROM manager \
-    where gatePass = '" + password + "';", False)
-    
+    rets = a.execqry("select tempManLeague from manager \
+	where gatePass='" + password + "';", False);
+	
     for ret in rets:
         stringed = ''.join(map(str,ret))
     
-	#rets = x.execqry("select tempManLeague from manager where tempManager=" + managerid + ";", False);
+    id = stringed	
+    
+    rets = a.execqry("select league_id from league \
+    where name='" + league + "';", False);	
+
+    for ret in rets:
+        stringed = ''.join(map(str,ret))
+    
+	id1 = stringed
 	
-    result = []	
-    if stringed != 'None':
-        rets = a.execqry("SELECT tempManLeague FROM manager \
-        where gatepass = '" + password + "';", False)
-        for ret in rets:
-            stringed = map(str,ret)
-            result.append(stringed)
+	result = []	
+    if id == id1:
+        result.append(id)
     else:
         result.append("None")
 		
