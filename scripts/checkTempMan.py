56343ad30bfb050e6c75e86a2b79c2b46ecd1dc2
Kay Apao <kayapao@yahoo.com>
2014-10-11 15:45:38 +0800

    changed username parameter with managerid, remove select tempManager line and added new list named temp (checkTempMan.py)

--- scripts/checkTempMan.py
+++ scripts/checkTempMan.py
@@ -5,32 +5,36 @@ try:
 except ImportError:
     import simplejson as json
 
-def index(req, username):
-    username = cgi.escape(username) 
+def index(req, id):
+    id = cgi.escape(id) 
     x = doSql()
-    rets = x.execqry("SELECT manager_id FROM manager \
-    INNER JOIN users ON users.userid = manager.userid_fk \
-    where users.username = '" + username + "';", False)
     
-    for ret in rets:
-        stringed = ''.join(map(str,ret))
-
-    id = stringed
-	
-    rets = x.execqry("SELECT tempManager, first_name, last_name, gatePass from manager \
+    rets = x.execqry("SELECT first_name, last_name, gatePass from manager \
     where tempManager=" + id + ";" , False)
     
     result = []
+	
     for ret in rets:
         stringed = map(str,ret)
         result.append(stringed)
+		
+    rets = x.execqry("SELECT name from league INNER JOIN manager on league.managerid_fk = manager.manager_id \
+    where tempManager=" + id + ";" , False)	
+	
+    temp = []
+    for ret in rets:
+        stringed = ''.join(map(str,ret))
+        temp.append(stringed)
+    result.append(temp)	
 
     rets = x.execqry("SELECT username from users \
     INNER JOIN manager ON users.userid=manager.userid_fk \
     where tempManager=" + id + ";" , False)
 	
+    temp = []
     for ret in rets:
         stringed = ''.join(map(str,ret))
-        result.append(stringed)
+        temp.append(stringed)
+    result.append(temp)	
 	
     return json.dumps(result)
