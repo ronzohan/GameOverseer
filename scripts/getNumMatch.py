18858eeb7840e4743888ae33b2721501a03a73aa
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-10-12 11:37:48 +0800

    Addded script for getting full event info for the pagination

--- scripts/getNumMatch.py
+++ scripts/getNumMatch.py
@@ -10,7 +10,21 @@ def index(req,leagueidarray):
 
     leagueidarray = leagueidarray.replace('[', '(').replace(']', ')').replace('\"','\'')
     x = doSql()
-    rets = x.execqry("SELECT * FROM events WHERE leagueid_fk in "+leagueidarray+";", False)
+    rets = x.execqry("SELECT count(*) FROM events WHERE leagueid_fk in "+leagueidarray+" and scoret1 is null and scoret2 is null", False)
+    result = []
+    for ret in rets:
+        stringed = map(str, ret)
+        result.append(stringed)
+
+    return json.dumps(result)
+
+def getEventInfoByPage(req,leagueidarray,offset):
+    leagueidarray = cgi.escape(leagueidarray)
+    offset = cgi.escape(offset)
+
+    leagueidarray = leagueidarray.replace('[', '(').replace(']', ')').replace('\"','\'')
+    x = doSql()
+    rets = x.execqry("SELECT * FROM events WHERE leagueid_fk in "+leagueidarray+" and scoret1 is null and scoret2 is null ORDER by edate ASC limit 4 offset "+offset+" ;", False)
     result = []
     for ret in rets:
         stringed = map(str, ret)
